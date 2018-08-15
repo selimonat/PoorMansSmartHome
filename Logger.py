7782c34e6bac6fd788e1cae8fa6a40e7eb145225
selimonat <onatselim@gmail.com>
2018-08-15 14:06:43 +0200

    working on an implementation of GoPro Logger, not yet stable

--- Logger.py
+++ Logger.py
@@ -7,6 +7,70 @@ from numpy import savetxt, linspace, round
 import subprocess
 import time as t
 
+
+from goprocam import constants;
+from goprocam import GoProCamera;
+import subprocess
+import os
+import cv2
+import numpy as np
+import sys
+
+import logging
+logging.basicConfig(filename='/home/pi/motion_detection.log',level=logging.DEBUG,format='%(message)s')
+
+def log_motion():
+	#open camera and change photo mode to smallest size
+	gpCam = GoProCamera.GoPro(constants.auth)
+	gpCam.sendCamera(constants.Hero3Commands.PHOTO_RESOLUTION,constants.Hero3Commands.PhotoResolution.PR5MP_W)
+	
+	save_path = "/home/pi/motion_detection/%s.jpg"
+	tmp_path  = "/tmp/%s.jpg"
+	
+	filename = [None];
+	for i in [0, 1]:
+		filename.append(tmp_path % (i))
+		print("Taking photo %d called %s" % (i, filename[-1]))
+		gpCam.take_photo()
+		t.sleep(2)
+		print("Downloading photo %d" % i)
+		gpCam.downloadLastMedia(custom_filename=str(filename[-1]))
+		print("Reading photo %d" % i)
+		img = np.float32(cv2.imread(filename[-1],cv2.IMREAD_GRAYSCALE))
+		if i == 0:
+			out = img
+		else:
+			out = out - img
+		t.sleep(10)
+	
+	print("Computing motion power")
+	out = abs(out);
+	print("Smoothing motion power")
+	out = cv2.GaussianBlur(out,(61,61),25)
+	#out = out/np.amax(out)
+	#out = out*255
+	print("Saving diff image")
+	filename.append(save_path % (str(round(t.time()))))
+	cv2.imwrite(filename[-1],np.uint8(out),[int(cv2.IMWRITE_JPEG_QUALITY), 10])
+	
+	motion_power = np.amax(out)
+	print("Motion Value: %s" % (motion_power))
+    
+	out = str(motion_power) + " " + str(t.strftime("%a %d %b %H:%M:%S CET %Y")) + " " + str(round(t.time()))
+	
+	logging.info(out)
+	print("Montage collected data")
+	cmd = "/usr/bin/montage %s %s %s -geometry +0+0 -resize 25%% -background none  %s" % (filename[1],filename[2],filename[3],filename[3])
+	cmd = cmd.split()
+	t.sleep(2)
+	subprocess.call(cmd)
+
+	if motion_power > 30:
+		print("sending results as an email")
+		subprocess.Popen(["/home/pi/code/shell/bin/SendLastPic.sh","/home/pi/motion_detection/","Motion value: " + str(motion_power),"Motion Detected (>20)","onatselim@gmail.com"])
+    
+	return motion_power
+
 def log_audio():
     """ Takes a snapshot of the auditory landscape, computes its Fourier
     transform, and saves the result to the log file with its time stamp. 
