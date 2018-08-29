606005d781e6a7624f4855c42b719abc5c659200
selimonat <onatselim@gmail.com>
2018-08-29 09:52:28 +0200

    work in progress. new method that adds div, script to current state dict

--- Plotter.py
+++ Plotter.py
@@ -6,6 +6,57 @@ from scipy.stats import zscore
 
 from bokeh.plotting import figure, output_file, show 
 from bokeh.embed import components
+from bokeh.transform import linear_cmap
+
+def df_to_bar(d):
+    """
+		d is dict form of current_state df.
+    """
+	for k1 in d.keys():
+		for k2 in d[k1].keys():
+			for k3 in d[k1][k2].keys():
+				script, div = P.df_to_bar()
+				d[k1][k2][k3].update({'div': div})
+				d[k1][k2][k3].update({'script': script})
+	return d
+
+def df_to_bar():
+    """
+        data is a dict with x and y keys.
+    """
+
+    #output_file = "/tmp/page.html"
+    p = figure(tools="pan,box_zoom,reset,save",
+               title="test",
+               x_axis_label="x",
+               y_axis_label="y",
+               plot_width=800,
+               plot_height=200,            
+               x_axis_type="datetime")
+    p.grid.visible=False
+    X=[1,2,3,4]
+    Y=[3,4,5,6]
+    p.vbar(x=X,
+           top=Y,
+           width=1,
+           bottom=0,
+           color="black")
+           #fill_color=linear_cmap(Y,'Viridis256',0,max(Y)),
+          #)
+
+    script, div = components(p)
+    return script, div
+
+def historical_viewa(current_state):
+    colors = ["#75968f", "#a5bab7", "#c9d9d3", "#e2e2e2", "#dfccce", "#ddb7b1", "#cc7878", "#933b41", "#550b1d"]
+    TOOLS = "hover,save,pan,box_zoom,reset,wheel_zoom"
+    
+    p = figure(title="US Unemployment ({0} - {1})".format(years[0], years[-1]),
+           x_range=years, y_range=list(reversed(months)),
+           x_axis_location="above", plot_width=900, plot_height=400,
+           tools=TOOLS, toolbar_location='below',
+           tooltips=[('date', '@Month @Year'), ('rate', '@rate%')])
+
 
 def df_to_histogram(df,cols="all",normalize=False):
     """
